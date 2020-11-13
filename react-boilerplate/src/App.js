f779bfe1888060ac898390f39193a3613f6747eb
Siva Kumar <siva9@cisco.com>
2020-11-13 16:30:15 +0530

    react-boilerplate

---
+++ react-boilerplate/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => (
+	<div className="container">
+		<h1>Hello World, React!</h1>
+		<button className="btn btn-primary">Click</button>
+	</div>
+)
+
+export default App;
